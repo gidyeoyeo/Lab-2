c33259f3d9bae591e9038eb8501205ddcffc2c3b
Gideon <JWYEO.23@ichat.sp.edu.sg>
2024-04-29 11:03:04 +0800

    Changes in code

--- bmi.py
+++ bmi.py
@@ -5,8 +5,12 @@ def calculate_bmi(height, weight):
     print("bmi = "+ str(bmi))  
     if bmi<18.5:
         print("under weight")
+        return -1
+
     elif bmi >25.0:
         print("over weight")
+        return 1
     else:
         print("NOrmal weight")
+        return 0
 calculate_bmi(weight=57, height=1.73)
